40ac27da107af3ce9236e747f2399f7dbc540d8f
smrutipachpute <smruti.pachpute1404@gmail.com>
2024-05-20 20:27:17 +0530

    Remove accidentally added files from the secondary branch

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Define the base directory
-BASE_DIR = os.path.abspath(os.path.dirname(__name__))
-
-# Configuration options
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-SECRET_KEY = 'your_secret_key'
